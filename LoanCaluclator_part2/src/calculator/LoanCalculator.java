67ac5a894e35726842220a5d3143468c70bd644f
mrcooper <mrcooper@DESKTOP-B1B3THB.attlocal.net>
2015-11-06 23:12:17 -0500

    added comments

--- LoanCaluclator_part2/src/calculator/LoanCalculator.java
+++ LoanCaluclator_part2/src/calculator/LoanCalculator.java
@@ -1,22 +1,43 @@
 package calculator;
 
+/**
+ * @author micah.cooper
+ *
+ */
 public class LoanCalculator {
 	static int termInMonths;
 	static double monthlyInterestRate;
 	static double monthlyPayment;
 	
+	/**
+	* @param termInYears the number of years a loan has
+	* @return the result of turning years into months
+	**/
 	public static int getTermInMonths(int termInYears){
 		return 12*termInYears;
 	}
 	
+	/**
+	* @param the annual interest rate
+	* @return the monthly interest rate
+	**/
 	public static double getMonthlyInterestRate(double apr){
 		return (apr/100)/12;
 	}
 	
+	/**
+	* @param original amount loand
+	* @param the monthly interest rate
+	* @param total number of months
+	* @return the monthly payment amount
+	**/
 	public static double getMonthlyPayment(int amountLoaned, double monthlyRate, int totalMonths){
 		return (amountLoaned * monthlyRate) / (1 - (Math.pow( (1 + monthlyRate), (-1*totalMonths)) ));
 	}
 	
+	/**
+	* 
+	**/
 	public static void printValues(){
 		System.out.println("Number of months: "+termInMonths);
 		System.out.println("Monthly interest rate: "+monthlyInterestRate);
