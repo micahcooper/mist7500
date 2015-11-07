b419760130d008b67fb51aafe74f074b8f10b56b
mrcooper <mrcooper@DESKTOP-B1B3THB.attlocal.net>
2015-11-06 21:09:13 -0500

    Test upload

---
+++ LoanCalculator_part1/src/calculator/LoanCalculator.java
@@ -0,0 +1,22 @@
+package calculator;
+ 
+public class LoanCalculator {
+ 
+        public static void main(String[] args) {
+                // TODO Auto-generated method stub
+                int totalMonths;
+                double monthlyPayment;
+                double monthlyRate;
+               
+                double amountLoaned = Double.parseDouble(args[0]);
+                double interestRate = Double.parseDouble(args[1]);
+                int termInYears = Integer.parseInt(args[2]);
+               
+                monthlyRate = interestRate / 12;
+                totalMonths = termInYears * 12;
+                monthlyPayment = (amountLoaned * monthlyRate) / (1 - (Math.pow( (1 + monthlyRate), -totalMonths) ));
+               
+                System.out.println(monthlyPayment);
+        }
+ 
+}
